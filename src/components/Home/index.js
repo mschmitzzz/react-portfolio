74c64b4eea528320e3a76ac212a2658e374d9b91
Mac Schmitz <mac.schmitz@att.net>
2022-07-26 10:18:35 -0400

    commented out useState which breaks home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,23 +5,32 @@ import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    let myName = 'ac Schmitz'
-    const nameArray = myName.split("")
-    let myJob = "web developer."
-    const jobArray = myJob.split("")
+    // const [letterClass, setLetterClass] = useState('text-animate')
+    // let myName = 'ac Schmitz'
+    // const nameArray = myName.split("")
+    // let myJob = "web developer."
+    // const jobArray = myJob.split("")
 
-    useEffect(() => {
-        return setTimeout(() => {
-          setLetterClass('text-animate-hover')
-        }, 4000)
-      }, [])
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //       setLetterClass('text-animate-hover')
+    //     }, 4000)
+    //   }, [])
 
     return (
         <>
             <div className="container home-page">
                 <div className="text-zone">
                     <h1>
+                        Hi,
+                        <br />
+                        I'm 
+                        <img src={LogoTitle} alt="web developer name" />
+                        ac Schmitz
+                        <br />
+                        web developer.
+                    </h1>
+                {/* <h1>
                         <span className={letterClass}>H</span>
                         <span className={`${letterClass} _12`}>i,</span>
                         <br />
@@ -39,7 +48,7 @@ const Home = () => {
                     stringArray={jobArray} 
                     idx={22}
                     />
-                    </h1>
+                    </h1> */}
                     <h2>Fullstack Developer / JavaScript / Drummer</h2>
                     <Link to="/contact" className='flat-button'>CONTACT ME</Link>
                 </div>
