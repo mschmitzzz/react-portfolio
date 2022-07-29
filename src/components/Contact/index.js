3e38218b8df20d2e0c2397d519fda6761ce3943e
Mac Schmitz <mac.schmitz@att.net>
2022-07-29 18:42:25 -0400

    completed contact form functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,12 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
 		const timer = setTimeout(() => {
@@ -12,6 +14,26 @@ const Contact = () => {
 		}, 3000);
 		return () => clearTimeout(timer);
 	}, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'contact_service',
+            'template_4lb8pr2',
+            refForm.current,
+            'f7hglstdxDoOEMYGw'
+        )
+        .then(
+            () => {
+                alert('Message successfully sent')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again.')
+            }
+        )
+    }
 
     return (
         <>
@@ -22,7 +44,7 @@ const Contact = () => {
                 </h1>
                 <p>I am seeking new opportunities as a full-stack developer - open to both full-time positions and freelance work. Additionally, if you have any other questions, requests, or comments, please don't hesitate to contact me using the form below.</p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
                                 <input type='text' name='name' placeholder='Name' required />
