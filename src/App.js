7399835ff5b9d4bdb3e7441f17cec82c5483e16f
Mac Schmitz <mac.schmitz@att.net>
2022-08-01 19:13:19 -0400

    added path /react-portfolio to all routes

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/react-portfolio" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/react-portfolio/about" element={<About />} />
+      <Route path="/react-portfolio/contact" element={<Contact />} />
+      <Route path="/react-portfolio/portfolio" element={<Portfolio />} />
       </Route>
     </Routes>
     </>
